eec5b70de280ae32cb6bb5961fed09f1210aab18
Z <z@mysoft.tech>
2021-02-07 23:16:09 +0800

    default_resource_finalizer<HANDLE>

---
+++ include/zed/win/handled_resource.hpp
@@ -0,0 +1,28 @@
+#pragma once
+// -------------------------------------------------
+// ZED Kit
+// -------------------------------------------------
+//   File Name: handled_resource.hpp
+//      Author: Ziming Li
+//     Created: 2021-02-06
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef ZED_WIN_HANDLED_RESOURCE_HPP
+#define ZED_WIN_HANDLED_RESOURCE_HPP
+
+#include <Windows.h>
+#include "../memory.hpp"
+
+namespace zed {
+
+template <>
+struct default_resource_finalizer<HANDLE>
+{
+    void operator()(HANDLE h) const { ::CloseHandle(h); }
+};
+
+} // namespace zed
+
+#endif // ZED_WIN_HANDLED_RESOURCE_HPP
