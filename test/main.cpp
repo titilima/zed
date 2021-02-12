b91baaaade8a1397a28aecc6fe1ef6110323d7f8
Z <z@mysoft.tech>
2021-02-13 00:34:26 +0800

    Unit test.

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+// -------------------------------------------------
+// ZED Kit - Unit tests
+// -------------------------------------------------
+//   File Name: main.hpp
+//      Author: Ziming Li
+//     Created: 2021-02-13
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
+#include <gtest/gtest.h>
+#include "zed/string/algorithm.hpp"
+
+int main(int argc, char *argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
