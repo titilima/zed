af305aac69a39fed150d05be49ed3287508d35a5
Z <z@mysoft.tech>
2021-02-13 01:00:23 +0800

    Update.

--- test/main.cpp
+++ test/main.cpp
@@ -11,6 +11,15 @@
 #include <gtest/gtest.h>
 #include "zed/string/algorithm.hpp"
 
+TEST(StringComparisons, ComparesCorrectly)
+{
+    ASSERT_TRUE(zed::strequ("FOO", "FOO"));
+    ASSERT_FALSE(zed::strequ("FOO", "foo"));
+    ASSERT_FALSE(zed::strequ("Accept", "Accept-Language"));
+    ASSERT_TRUE(zed::striequ("FOO", "foo"));
+    ASSERT_NE(zed::stricmp("_", "A"), zed::stricmp("_", "a"));
+}
+
 int main(int argc, char *argv[])
 {
     testing::InitGoogleTest(&argc, argv);
