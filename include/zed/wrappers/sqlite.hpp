ee15d55d76a9a8900afe382f84ba9b85924f8495
Z <z@mysoft.tech>
2021-03-14 00:27:44 +0800

    Update helpers.

--- include/zed/wrappers/sqlite.hpp
+++ include/zed/wrappers/sqlite.hpp
@@ -34,22 +34,75 @@ struct default_resource_finalizer<sqlite3_stmt *>
 class sqlite_stmt : unique_resource<sqlite3_stmt *>
 {
 public:
+    sqlite_stmt(void) : unique_resource() {}
+    sqlite_stmt(sqlite_stmt &&o) : unique_resource(std::move(o)) {}
     ~sqlite_stmt(void) = default;
+
+    using unique_resource::operator bool;
+    using unique_resource::swap;
 
     void reset(void) { ::sqlite3_reset(get()); }
     int step(void) { return ::sqlite3_step(get()); }
 
+    bool bind(int one_based_index, int n);
     template <class String>
     bool bind(int one_based_index, const String &s, bool transient = false) { return bind(one_based_index, s.data(), s.length(), transient); }
     bool bind(int one_based_index, const char *psz, bool transient = false) { return bind(one_based_index, psz, -1, transient); }
+    template <class Container>
+    bool bind_blob(int one_based_index, const Container &c, bool transient = false) { return bind_blob(one_based_index, c.data(), c.size(), transient); }
+    bool bind_blob(int one_based_index, const void *data, size_t size, bool transient = false);
+    bool bind_int64(int one_based_index, sqlite_int64 n);
+    bool bind_null(int one_based_index);
+    template <class String>
+    bool bind_string_or_null(int one_based_index, const String &s, bool transient = false);
 
     int get_column_int(int zero_based_index) { return ::sqlite3_column_int(get(), zero_based_index); }
+    sqlite_int64 get_column_int64(int zero_based_index) { return ::sqlite3_column_int64(get(), zero_based_index); }
     std::string get_column_text(int zero_based_index);
 private:
     friend class sqlite;
+    friend class sqlite_qstream;
     explicit sqlite_stmt(sqlite3_stmt *stmt);
 
     bool bind(int one_based_index, const char *ps, int l, bool transient);
+};
+
+class sqlite_rstream
+{
+public:
+    operator bool() const { return SQLITE_ROW == m_ret; }
+private:
+    friend class sqlite_qstream;
+    friend const sqlite_rstream& operator>>(const sqlite_rstream &rs, int &n);
+    friend const sqlite_rstream& operator>>(const sqlite_rstream &rs, std::string &s);
+    explicit sqlite_rstream(sqlite_stmt &stmt);
+
+    int index(void) const;
+
+    int m_ret = SQLITE_DONE;
+    mutable sqlite_stmt m_stmt;
+    mutable int m_index = 0;
+};
+
+class sqlite_qstream
+{
+public:
+    ~sqlite_qstream(void);
+
+    operator sqlite_rstream() const { return sqlite_rstream(m_stmt); }
+
+    int flush(void);
+    void restore(sqlite_rstream &rst);
+private:
+    friend class sqlite;
+    friend const sqlite_qstream& operator<<(const sqlite_qstream &qs, int n);
+    friend const sqlite_qstream& operator<<(const sqlite_qstream &qs, const std::string &s);
+    explicit sqlite_qstream(sqlite3_stmt *stmt);
+
+    int index(void) const;
+
+    mutable sqlite_stmt m_stmt;
+    mutable int m_index = 1;
 };
 
 class sqlite : unique_resource<sqlite3 *>
@@ -61,10 +114,13 @@ public:
     void close(void) { reset(); }
 
     bool exec(const char *sql);
+    sqlite_qstream query(const char *sql);
 
     template <class String>
     sqlite_stmt prepare(const String &sql) { return sqlite_stmt(prepare_internal(sql.data(), sql.length())); }
     sqlite_stmt prepare(const char *sql) { return sqlite_stmt(prepare_internal(sql, -1)); }
+
+    auto last_insert_rowid(void) { return ::sqlite3_last_insert_rowid(get()); }
 
     class transaction;
 private:
@@ -122,6 +178,11 @@ inline sqlite3_stmt* sqlite::prepare_internal(const char *sql, int l)
     return ret;
 }
 
+inline sqlite_qstream sqlite::query(const char *sql)
+{
+    return sqlite_qstream(prepare_internal(sql, -1));
+}
+
 inline sqlite::transaction::transaction(sqlite &db, const char *name) : m_db(db)
 {
     if (nullptr != name && '\0' != *name)
@@ -153,15 +214,118 @@ inline void sqlite::transaction::rollback(void)
     m_processed = true;
 }
 
+inline sqlite_qstream::sqlite_qstream(sqlite3_stmt *stmt) : m_stmt(stmt)
+{
+}
+
+inline sqlite_qstream::~sqlite_qstream(void)
+{
+    if (m_stmt && m_index > 1)
+        m_stmt.step();
+}
+
+inline int sqlite_qstream::flush(void)
+{
+    if (m_stmt && m_index > 1)
+    {
+        m_index = 0;
+        return m_stmt.step();
+    }
+    return SQLITE_ERROR;
+}
+
+inline int sqlite_qstream::index(void) const
+{
+    if (0 == m_index)
+    {
+        m_stmt.reset();
+        ++m_index;
+    }
+    return m_index++;
+}
+
+inline void sqlite_qstream::restore(sqlite_rstream &rst)
+{
+    m_index = 0;
+    m_stmt.swap(rst.m_stmt);
+}
+
+inline const sqlite_qstream& operator<<(const sqlite_qstream &qs, int n)
+{
+    qs.m_stmt.bind(qs.index(), n);
+    return qs;
+}
+
+inline const sqlite_qstream& operator<<(const sqlite_qstream &qs, const std::string &s)
+{
+    qs.m_stmt.bind_string_or_null(qs.index(), s);
+    return qs;
+}
+
+inline sqlite_rstream::sqlite_rstream(sqlite_stmt &stmt) : m_stmt(std::move(stmt))
+{
+    m_ret = m_stmt.step();
+}
+
+inline int sqlite_rstream::index(void) const
+{
+    return m_index++;
+}
+
+inline const sqlite_rstream& operator>>(const sqlite_rstream &rs, int &n)
+{
+    n = rs.m_stmt.get_column_int(rs.index());
+    return rs;
+}
+
+inline const sqlite_rstream& operator>>(const sqlite_rstream &rs, std::string &s)
+{
+    s = rs.m_stmt.get_column_text(rs.index());
+    return rs;
+}
+
 inline sqlite_stmt::sqlite_stmt(sqlite3_stmt *stmt) : unique_resource(stmt)
 {
     ZASSERT(is_valid(get()));
+}
+
+inline bool sqlite_stmt::bind(int one_based_index, int n)
+{
+    int r = ::sqlite3_bind_int(get(), one_based_index, n);
+    return SQLITE_OK == r;
 }
 
 inline bool sqlite_stmt::bind(int one_based_index, const char *ps, int l, bool transient)
 {
     int r = ::sqlite3_bind_text(get(), one_based_index, ps, l, transient ? SQLITE_TRANSIENT : SQLITE_STATIC);
     return SQLITE_OK == r;
+}
+
+inline bool sqlite_stmt::bind_blob(int one_based_index, const void *data, size_t size, bool transient)
+{
+    int r = ::sqlite3_bind_blob(get(), one_based_index, data, size, transient ? SQLITE_TRANSIENT : SQLITE_STATIC);
+    return SQLITE_OK == r;
+}
+
+inline bool sqlite_stmt::bind_int64(int one_based_index, sqlite_int64 n)
+{
+    int r = ::sqlite3_bind_int64(get(), one_based_index, n);
+    return SQLITE_OK == r;
+}
+
+inline bool sqlite_stmt::bind_null(int one_based_index)
+{
+    int r = ::sqlite3_bind_null(get(), one_based_index);
+    return SQLITE_OK == r;
+}
+
+template <class String>
+bool sqlite_stmt::bind_string_or_null(int one_based_index, const String &s, bool transient)
+{
+    if (s.empty())
+        return bind_null(one_based_index);
+    else
+        return bind<String>(one_based_index, s, transient);
 }
 
 inline std::string sqlite_stmt::get_column_text(int zero_based_index)
