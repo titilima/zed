0ea6acd13233fe8d1e5bdfcaf4cdd35b6385c063
Z <z@mysoft.tech>
2021-03-16 21:49:43 +0800

    parser_stream::parsed_count

--- include/zed/string/parser.hpp
+++ include/zed/string/parser.hpp
@@ -22,36 +22,41 @@ class parser_stream
 public:
     virtual ~parser_stream(void) = default;
 
-    virtual int current_char(void) = 0;
+    virtual int current_char(void) const = 0;
     int peek(const char *chars_to_skip = ascii_whitespace<char>::chars);
 
     int advance(void);
+
+    size_t parsed_count(void) const { return m_current - m_start; }
+protected:
+    parser_stream(const char *start) : m_start(start), m_current(start) {}
+
+    const char *m_current;
 private:
     virtual void advance_internal(void) = 0;
+
+    const char *m_start;
 };
 
 class parser_psz_stream final : public parser_stream
 {
 public:
-    explicit parser_psz_stream(const char *psz) : m_psz(psz) {}
+    explicit parser_psz_stream(const char *psz) : parser_stream(psz) {}
 private:
-    int current_char(void) override;
+    int current_char(void) const override;
     void advance_internal(void) override;
-
-    const char *m_psz;
 };
 
 class parser_string_stream final : public parser_stream
 {
 public:
     template <class String>
-    explicit parser_string_stream(const String &s) : m_ps(s.data()), m_left(s.length()) {}
+    explicit parser_string_stream(const String &s) : parser_stream(s.data()), m_end(s.data() + s.length()) {}
 private:
-    int current_char(void) override { return m_left > 0 ? *m_ps : EOF; }
+    int current_char(void) const override { return m_current < m_end ? *m_current : EOF; }
     void advance_internal(void) override;
 
-    const char *m_ps;
-    size_t m_left;
+    const char *m_end;
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -76,22 +81,22 @@ inline int parser_stream::peek(const char *chars_to_skip)
     return ch;
 }
 
-inline int parser_psz_stream::current_char(void)
+inline int parser_psz_stream::current_char(void) const
 {
-    char ch = *m_psz;
+    char ch = *m_current;
     return '\0' != ch ? ch : EOF;
 }
 
 inline void parser_psz_stream::advance_internal(void)
 {
-    ZASSERT('\0' != *m_psz);
-    ++m_psz;
+    ZASSERT('\0' != *m_current);
+    ++m_current;
 }
 
 inline void parser_string_stream::advance_internal(void)
 {
-    ZASSERT(m_left > 0);
-    ++m_ps; --m_left;
+    ZASSERT(m_current < m_end);
+    ++m_current;
 }
 
 } // namespace zed
