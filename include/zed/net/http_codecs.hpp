0da7dc3808462903ebdf4c2241ca5288b1d0cebc
Z <z@mysoft.tech>
2021-03-19 18:28:39 +0800

    encode_uri_component

--- include/zed/net/http_codecs.hpp
+++ include/zed/net/http_codecs.hpp
@@ -13,6 +13,7 @@
 #define ZED_NET_HTTP_CODECS_HPP
 
 #include <string>
+#include "../ctype.hpp"
 #include "../string.hpp"
 
 namespace zed {
@@ -20,10 +21,35 @@ namespace zed {
 std::string decode_uri_component(const char *psz);
 std::string decode_uri_component(const string_piece<char> &s);
 
+std::string encode_uri_component(const char *psz);
+std::string encode_uri_component(const string_piece<char> &s);
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Implementations
 
 namespace detail {
+
+inline void append_encoded(std::string &s, char ch)
+{
+    constexpr char special_chars[] = "-_.!~*'()";
+    constexpr char hex_digits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+    if (zed::isdigit(ch) || zed::isalpha(ch) || nullptr != strchr(special_chars, ch))
+    {
+        s.push_back(ch);
+    }
+    else if (' ' == ch)
+    {
+        s.push_back('+');
+    }
+    else
+    {
+        unsigned char b = static_cast<unsigned char>(ch);
+        s.push_back('%');
+        s.push_back(hex_digits[(b >> 4)]);
+        s.push_back(hex_digits[(b & 0xf)]);
+    }
+}
 
 template <class CharIterator>
 std::string decode_uri_component(CharIterator &it)
@@ -70,6 +96,18 @@ std::string decode_uri_component(CharIterator &it)
     return ret;
 }
 
+template <class CharIterator>
+std::string encode_uri_component(CharIterator &it)
+{
+    std::string ret;
+    while (!it.reach_the_end())
+    {
+        append_encoded(ret, *it);
+        ++it;
+    }
+    return ret;
+}
+
 } // namespace detail
 
 inline std::string decode_uri_component(const char *psz)
@@ -82,6 +120,18 @@ inline std::string decode_uri_component(const string_piece<char> &s)
 {
     detail::char_iterator it(s);
     return detail::decode_uri_component(it);
+}
+
+inline std::string encode_uri_component(const char *psz)
+{
+    detail::char_iterator it(psz);
+    return detail::encode_uri_component(it);
+}
+
+inline std::string encode_uri_component(const string_piece<char> &s)
+{
+    detail::char_iterator it(s);
+    return detail::encode_uri_component(it);
 }
 
 } // namespace zed
